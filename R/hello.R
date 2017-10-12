85d5421d30c05cb27dfaea7767148a276ae70eca
fawuuu <fan.wu@spc.ox.ac.uk>
2017-10-12 20:24:33 +0100

    this is a message

--- R/hello.R
+++ R/hello.R
@@ -13,6 +13,30 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, wkkkkkorld wohssssso!")
+#Initializations for testing
+#in this example x=(x1,x2,theta1,theta2)
+del1 = function(x){
+  return(max(0, x[3]*exp(-(x[1]+x[2])^2)))
+}#max 1
+
+del2 = function(x){
+  return(x[4]*dcauchy(x[2]))
+}#max <0.32
+
+#intensities as list of functions
+#global bounds as vector
+#state includes both xsi and theta
+poispro = function(state, intensities, bounds){
+  d = length(intensities)
+  flip = FALSE
+  arrivals = rexp(d, bounds)
+  tau = min(arrivals)
+  ind = which.min(arrivals)
+  newstate = state
+  newstate[1:d] = state[1:d] + tau*state[(d+1):(2*d)]
+  if(runif(1)<intensities[[ind]](newstate)/bounds[ind]){
+    newstate[d+ind] = -newstate[d+ind]
+    flip = TRUE
+  }
+  return(list(state=newstate, time=tau, index=ind, flip=flip))
 }
