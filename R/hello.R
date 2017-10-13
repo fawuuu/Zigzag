0666aab1ae7a3a2653bafe1c88a05693615d3b57
fawuuu <fan.wu@spc.ox.ac.uk>
2017-10-13 15:54:21 +0100

    alg2

--- R/hello.R
+++ R/hello.R
@@ -14,33 +14,55 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 #Initializations for testing
-#in this example x=(x1,x2,theta1,theta2)
-del1 = function(x){
-  return(max(0, x[3]*exp(-(x[1]+x[2])^2)))
-}#max 1
+del = function(x){
+  return(2*x/(1+x^2))
+}# max =1rep(1,)
 
-del2 = function(x){
-  return(x[4]*dcauchy(x[2]))
-}#max <0.32
-
-#intensities as list of functions
+#derivatives as function with vector output
 #global bounds as vector
 #state includes both xsi and theta
-poispro_global = function(state, intensities, bounds){
-  d = length(intensities)
+poispro_global = function(state, derivatives, bounds){
+  d = length(state)/2
   flip = FALSE
   arrivals = rexp(d, bounds)
+  print(arrivals)
   tau = min(arrivals)
   ind = which.min(arrivals)
   newstate = state
   newstate[1:d] = state[1:d] + tau*state[(d+1):(2*d)]
-  if(runif(1)<intensities[[ind]](newstate)/bounds[ind]){
+  if(runif(1)<max(0, state[d+ind]*derivatives(newstate[1:d])[ind])/bounds[ind]){
     newstate[d+ind] = -newstate[d+ind]
     flip = TRUE
   }
   return(list(state=newstate, time=tau, index=ind, flip=flip))
 }
 
-poispro_linear = function(state, intensities, bounds){
+#this should be outside of function call
+a = numeric(d); b = numeric(d)
+for(i in 1:d){
+  a[i] = state[d+i]*derivatives(state[1:d])[i]
+  b[i] = sqrt(d)*sqrt(sum(hessian[,i]^2))
+}
+
+poispro_hessian = function(state, derivatives, a, b){
+  d = length(state)/2
+  flip = FALSE
+  arrivals =  -a/b + sqrt((a/b)^2 + 2/b*(-log(runif(d))))
+  tau = min(arrivals)
+  ind = which.min(arrivals)
+  a = a + b*tau
+  newstate = state
+  newstate[1:d] = state[1:d] + tau*state[(d+1):(2*d)]
+  a_temp = state[d+ind]*derivatives(newstate[1:d])[ind]
+  print(max(0, a_temp)/max(0, a[ind]))
+  if(runif(1)<max(0, a_temp)/max(0, a[ind])){
+    newstate[d+ind] = -newstate[d+ind]
+    flip = TRUE
+  }
+  a[ind] = a_temp*(-1)^as.numeric(flip)
+  return(list(state=newstate, time=tau, index=ind, flip=flip, a=a))
+}
+
+poispro_subsamp = function(state, derivatives){
 
 }
